875ee86d5fb03cef5ef3723f6696892f365497e3
Diego Yanez <alexanderyanez21@gmail.com>
2026-05-12 12:10:04 -0500

    Crud completo de libros

--- routes/libro_routes.py
+++ routes/libro_routes.py
@@ -4,6 +4,33 @@ from flask import Blueprint, jsonify, request
 from services import libro_service
 
 # =========================================================
+# Este archivo contiene las rutas HTTP del módulo libros.
+#
+# Aquí se definen los endpoints de la API:
+#
+# POST   /libros       -> Crear libro
+# GET    /libros       -> Obtener todos los libros
+# GET    /libros/<id>  -> Obtener un libro por ID
+# PUT    /libros/<id>  -> Actualizar libro
+# DELETE /libros/<id>  -> Eliminar libro
+#
+# RESPONSABILIDADES DE ESTE ARCHIVO:
+# ✅ Recibir peticiones HTTP
+# ✅ Leer datos JSON enviados por el cliente
+# ✅ Llamar al service correspondiente
+# ✅ Devolver respuestas JSON
+# ✅ Manejar códigos HTTP
+#
+# ESTE ARCHIVO NO DEBE:
+# ❌ Contener lógica de negocio
+# ❌ Acceder directamente a SQLAlchemy
+# ❌ Hacer consultas a la base de datos
+#
+# En arquitectura SOLID:
+# Este archivo representa la CAPA DE PRESENTACIÓN.
+# =========================================================
+
+# =========================================================
 # BLUEPRINT
 # =========================================================
 # Un Blueprint permite separar rutas por módulos.
@@ -29,39 +56,168 @@ desde el cliente (FrontEnd - Angular | Insomnia para pruebas de API )
 
 """
 
+#CRUD de libros
+
+# Crear un libro
 @libros_bp.route('/libros', methods=['POST'])
-
-
 def crear_libro():
-    """
-    request.get_json()
 
-    Captura el Json enviado por el cliente. 
-
-    Ejemplo de Json : 
-    {
-        "titulo": "Clean Code",
-        "autor": "Robert C. Martin"
-    }
-    """
     data = request.get_json()
 
-    libro_creado = libro_service.crear_libro(data)
+    resultado = libro_service.crear_libro(data)
 
-    if libro_creado['exito']:
+    # =====================================================
+    # RESPUESTA EXITOSA
+    # =====================================================
+    if resultado['exito']:
         return jsonify({
-            "exito": True,
-            "mensaje": "Libro creado exitosamente",
-
-            
-            #Convertir esto a JSON
-            "libro": libro_creado['libro'].to_dict()
+            "mensaje": "Libro creado correctamente",
+            "libro": resultado['libro'].to_dict()
         }), 201
     
-    return jsonify({
-        "exito": False,
-        "mensaje": "Error al crear el libro"
-    }), 400
+    # =====================================================
+    # RESPUESTA DE ERROR
+    # =====================================================  
+    else:
+        return jsonify({
+            "mensaje": "Error al crear el libro",
+            "error": resultado['error']
+        }), 500 
+
+
+
+# =========================================================
+# OBTENER TODOS LOS LIBROS
+# =========================================================
+# Endpoint:
+# GET /libros
+#
+# Devuelve todos los libros registrados.
+# =========================================================
+
+# =========================================================
+# OBTENER TODOS LOS LIBROS
+# =========================================================
+@libros_bp.route('/libros', methods=['GET'])
+def obtener_libros():
+
+    resultado = libro_service.obtener_libros()
+
+    # =====================================================
+    # RESPUESTA EXITOSA
+    # =====================================================
+    if resultado['exito']:
+
+        return jsonify({
+
+            "libros": resultado['libros']
+
+        }), 200
+
+    # =====================================================
+    # RESPUESTA DE ERROR
+    # =====================================================
+    else:
+
+        return jsonify({
+
+            "mensaje": "Error al obtener libros",
+
+            "error": resultado['error']
+
+        }), 500
+
+@libros_bp.route('/libros/<int:id>', methods=['GET'])
+def obtener_libro(id):
+
+    resultado = libro_service.obtener_libro_por_id(id)
+
+    # =====================================================
+    # RESPUESTA EXITOSA
+    # =====================================================
+    if resultado['exito']:
+        return jsonify({
+            "libro": resultado['libro'].to_dict()
+        }), 200
+    
+    # =====================================================
+    # RESPUESTA DE ERROR
+    # =====================================================  
+    else:
+        return jsonify({
+            "mensaje": "Libro no encontrado",
+            "error": resultado['error']
+        }), 404
+
+# =========================================================
+# ACTUALIZAR LIBRO
+# =========================================================
+# Endpoint:
+# PUT /libros/<id>
+#
+# Permite modificar un libro existente.
+# =========================================================
+@libros_bp.route('/libros/<int:id>', methods=['PUT'])
+def actualizar_libro(id):
+
+    # =====================================================
+    # Obtener JSON enviado desde cliente (insomnia, postman...)
+    # ====================================================
+    data = request.get_json()
+
+    # =====================================================
+    # Enviar datos al service
+    # =====================================================
+    resultado = libro_service.actualizar_libro(id, data)
+
+    # =====================================================
+    # RESPUESTA EXITOSA
+    # =====================================================
+    if resultado['exito']:
+        return jsonify({
+            "mensaje": "Libro actualizado correctamente",
+            "libro": resultado['libro'].to_dict()
+        }), 200
+    
+    # =====================================================
+    # RESPUESTA DE ERROR
+    # =====================================================  
+    else:
+        return jsonify({
+            "mensaje": "Error al actualizar el libro",
+            "error": resultado['error']
+        }), 404
+
+# =========================================================
+# ELIMINAR LIBRO
+# =========================================================
+# Endpoint:
+# DELETE /libros/<id>
+#
+# Elimina un libro de PostgreSQL.
+# =========================================================
+@libros_bp.route('/libros/<int:id>', methods=['DELETE'])
+def eliminar_libro(id):
+    
+    resultado = libro_service.eliminar_libro(id)
+
+    # =====================================================
+    # RESPUESTA EXITOSA
+    # =====================================================
+    if resultado['exito']:
+        return jsonify({
+            "mensaje": "Libro eliminado correctamente"
+        }), 200
+
+    # =====================================================
+    # RESPUESTA DE ERROR
+    # =====================================================    
+    else:
+        return jsonify({
+            "mensaje": "Error al eliminar el libro",
+            "error": resultado['error']
+        }), 404
+
 
 
 
--- services/libro_service.py
+++ services/libro_service.py
@@ -44,3 +44,176 @@ def crear_libro(data):
 
             "error": str(e)
         }
+
+# =========================================================
+# OBTENER TODOS LOS LIBROS (GET)
+# =========================================================
+
+def obtener_libros():
+
+    try:
+
+        libros = libro_repository.obtener_libros()
+
+        return {
+            "exito": True,
+            "libros": [libro.to_dict() for libro in libros]
+        }
+
+    except Exception as e:
+
+        return {
+            "exito": False,
+            "error": str(e)
+        }
+
+# =========================================================
+# OBTENER LIBRO POR ID (GET)    
+# =========================================================
+def obtener_libro_por_id(id):
+    
+    # =====================================================
+    # BUSCAR LIBRO
+    # =====================================================
+
+    libro = libro_repository.obtener_por_id(id)
+
+    # =====================================================
+    # VALIDAR EXISTENCIA
+    # =====================================================
+
+    if not libro: 
+        return {
+            "exito": False,
+            "error": "Libro no encontrado"
+        }
+    
+    # =====================================================
+    # RESPUESTA EXITOSA
+    # =====================================================
+    else:
+        return {
+
+            "exito": True,
+
+            "libro": libro
+
+        }
+
+# =========================================================
+# ACTUALIZAR LIBRO
+# =========================================================
+def actualizar_libro(id, data):
+    
+    try:
+        # =================================================
+        # BUSCAR LIBRO EXISTENTE
+        # =================================================
+        libro = libro_repository.obtener_por_id(id)
+        
+        # =================================================
+        # VALIDAR EXISTENCIA
+        # =================================================
+        if not libro:
+
+            return {
+
+                "exito": False,
+
+                "error": "Libro no encontrado"
+
+            }
+        
+        # =================================================
+        # ACTUALIZAR DATOS
+        # =================================================
+        libro.titulo = data['titulo']
+        libro.autor = data['autor']
+        libro.editorial = data['editorial']
+        libro.anio = data['anio']
+        libro.isbn = data['isbn']
+        libro.stock = data['stock']
+        libro.categoria_id = data['categoria_id']
+        libro.usuario_id = data['usuario_id']
+
+        # =================================================
+        # GUARDAR CAMBIOS
+        # =================================================
+        libro_repository.actualizar_libro()
+
+        # =================================================
+        # RESPUESTA EXITOSA
+        # =================================================
+        return {
+
+            "exito": True,
+
+            "libro": libro
+
+        }
+    
+    # =================================================
+    # MANEJO DE ERRORES (ACTUALIZAR)
+    # =================================================
+    except Exception as e:
+
+        return {
+
+            "exito": False,
+
+            "error": str(e)
+
+        }
+
+# =========================================================
+# ELIMINAR LIBRO
+# =========================================================
+def eliminar_libro(id):
+
+    try: 
+        # =====================================================
+        # BUSCAR LIBRO
+        # =====================================================
+        libro = libro_repository.obtener_por_id(id)
+        
+        # =================================================
+        # VALIDAR EXISTENCIA
+        # =================================================
+        if not libro:
+
+            return {
+
+                "exito": False,
+
+                "error": "Libro no encontrado"
+
+            }
+        
+        # =====================================================
+        # ELIMINAR LIBRO
+        # =====================================================
+        libro_repository.eliminar_libro(libro)
+
+        # =====================================================
+        # RESPUESTA EXITOSA
+        # =====================================================
+        return {
+
+            "exito": True,
+
+            "mensaje": "Libro eliminado correctamente"
+
+        }
+    
+    # =================================================
+    # MANEJO DE ERRORES (DELETE)
+    # =================================================
+    except Exception as e:
+        
+        return {
+
+            "exito": False,
+
+            "error": str(e)
+
+        }
